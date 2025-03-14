27b3c04d380c333022ab3a1a79e0e6e864cadc5f
EdmondaPanji <edmondapanji.48@gmail.com>
2025-03-14 12:58:38 +0700

    Resolve merge conflict on routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 
-// Halaman utama - langsung ke katalog produk
 Route::get('/', [ProductController::class, 'index']);
-
-// CRUD Produk
 Route::resource('products', ProductController::class);
 
 // Keranjang
@@ -15,7 +12,6 @@ Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
 Route::post('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
 Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
 
-// Duplikat route products.index (sebenernya udah di resource)
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
