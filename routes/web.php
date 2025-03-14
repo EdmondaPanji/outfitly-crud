a1489bd9f53d06c1faff696603cf4f34083e8235
EdmondaPanji <edmondapanji.48@gmail.com>
2025-03-14 11:21:41 +0700

    outfitly

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 
+// Halaman utama - langsung ke katalog produk
 Route::get('/', [ProductController::class, 'index']);
+
+// CRUD Produk
 Route::resource('products', ProductController::class);
 
 // Keranjang
@@ -12,6 +15,7 @@ Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
 Route::post('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
 Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
 
+// Duplikat route products.index (sebenernya udah di resource)
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
