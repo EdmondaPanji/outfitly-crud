6faa481aa7192707480b0d4dbc3c7777b7bc0d03
sinagarafhael <sinagarafhael@gmail.com>
2025-05-05 13:52:40 +0700

    back

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 
+// Halaman Produk
 Route::get('/', [ProductController::class, 'index'])->name('products.index');
 
+// Keranjang
 Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
 Route::post('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
 Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
